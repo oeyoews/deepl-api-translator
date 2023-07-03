b105f2f407681af7ed009fbbbb458a612f419655
oeyoews <jyao4783@gmail.com>
2023-07-03 19:09:45 +0800

    Automatic commit

--- dist/deepl.js
+++ dist/deepl.js
@@ -1,26 +1,20 @@
 function translateText() {
-  var DEEPL_API_KEY = "Your api key";
-  var textToTranslate = process.argv[2] || "你好吗?";
-  var targetLanguage = process.argv[3] || "en";
-  var url = "https://api-free.deepl.com/v2/translate";
-  var params = new URLSearchParams();
-  params.append("auth_key", DEEPL_API_KEY);
-  params.append("text", textToTranslate);
-  params.append("target_lang", targetLanguage);
-  fetch(url, {
-    method: "POST",
-    body: params,
-  })
-    .then(function (response) {
-      return response.json();
+    const DEEPL_API_KEY = "YOUR_API_KEY";
+    const textToTranslate = process.argv[2] || "你好吗?";
+    const targetLanguage = process.argv[3] || "en";
+    const url = "https://api-free.deepl.com/v2/translate";
+    const params = new URLSearchParams();
+    params.append("auth_key", DEEPL_API_KEY);
+    params.append("text", textToTranslate);
+    params.append("target_lang", targetLanguage);
+    fetch(url, {
+        method: "POST",
+        body: params,
     })
-    .then(function (data) {
-      var translatedText = data.translations[0].text;
-      console.log(
-        "Original Text: "
-          .concat(textToTranslate, "\nTranslated Text: ")
-          .concat(translatedText)
-      );
+        .then((response) => response.json())
+        .then((data) => {
+        const translatedText = data.translations[0].text;
+        console.log(`Original Text: ${textToTranslate}\nTranslated Text: ${translatedText}`);
     });
 }
 translateText();
